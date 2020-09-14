2e98aaed23d687135f490c3eedb59a4cfa058633
Eitan Lab <eitanlab@gmail.com>
2020-09-14 11:41:38 -0300

    Clase Restaurant lista

--- restaurant/restaurant/Program.cs
+++ restaurant/restaurant/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,24 @@ namespace restaurant
         static void Main(string[] args)
         {
             Restaurant Tegui = new Restaurant(10,18,5,true);
-            Console.WriteLine(Tegui.cantidadMesas);
-             
+            Tegui.agregarEmpleado("Juan");
+            Tegui.agregarEmpleado("Pepe");
+            Tegui.quitarEmpleado("Pepe");
+            bool empleadoQuitado = Tegui.quitarEmpleado("Gonzalo");
+            if(empleadoQuitado == true)
+            {
+                Console.WriteLine("Empleado quitado");
+            } else
+            {
+                Console.WriteLine("No se encontró empleado a quitar");
+            }
+            Tegui.agregarMesa();
+            Tegui.empleados.ForEach(Print);
+            void Print(string s)
+            {
+                Console.WriteLine(s);
+            }
+
         }
     }
 }
